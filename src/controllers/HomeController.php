000bcf35cc74b9673639bfa69223fcbadf031186
Luciano Nonato <lucianononato33@gmail.com>
2023-01-15 18:04:49 -0300

    Adicionando arquivos

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -5,30 +5,8 @@ use \core\Controller;
 use \src\models\Usuario;
 
 class HomeController extends Controller {
-
-    public function form() {
-        $this->render('form');
+    public function home() {
+        $this->render('home');
     }
 
-    public function formAction() {
-        $name = filter_input(INPUT_POST, 'name');
-        $email = filter_input(INPUT_POST, 'email');
-
-        if($name && $email){
-            $data = Usuario::select()->where('email', $email)->execute();
-
-            if(count($data)=== 0){
-                //insere
-                Usuario::insert([
-                    'nome' => $name,
-                    'email' => $email
-                ])->execute();
-                // redirect para /
-                $this->redirect('/form');
-            }
-        } 
-
-        //redirect para \form
-        $this->redirect('/form');
-    }
 } 
--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,5 @@ use core\Router;
 
 $router = new Router();
 
-$router->get('/', 'HomeController@index');
+$router->get('/home', 'HomeController@home');
 
-
-$router->get('/form', 'HomeController@form');
-$router->post('/form', 'HomeController@formAction');

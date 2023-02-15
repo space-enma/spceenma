98eeab97f34921973512166f87f74020604f43b9
Luciano Nonato <lucianononato33@gmail.com>
2023-02-15 18:41:18 -0300

    adicionando o site

---
+++ src/views/pages/contato.php
@@ -0,0 +1,9 @@
+<?php $render('header'); ?>
+
+<?php $render('../pages/formulario'); ?>
+
+
+<?php $render('footer'); ?>
+
+<script src="<?=$base;?>/assets/js/home.js"> </script>
+<script src="<?=$base;?>/assets/js/menu.js"> </script>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,19 +1,26 @@
 <html>
 <head>
     <title>Space Enma</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
 </head>
 <header>
+    <img class="fundo-img" src="<?=$base;?>/assets/img/planodefundo/fundomenu.jpg" alt="">  
     <div class="header">
         <div class="logo">
-            <img  class="img-logo" src="<?=$base;?>/assets/img/logospace.png" alt="">
+            <img  class="img-logo" src="<?=$base;?>/assets/img/logospace1.png" alt="">
         </div>
-        <div class="menu">
+        <div class="menu ">
+            <div class="menu-toggle ">
+                <div class="one"></div>
+                <div class="two"></div>
+                <div class="three"></div>
+            </div>
             <nav>
                 <ul>
-                    <li class="active"><a href="">Home</a> </li>
-                    <li><a href="">Serviços</a> </li>
-                    <li><a href="">Contato</a></li>
+                    <li class=""><a href="<?=$base;?>/home">Home</a> </li>
+                    <li><a  href="">Serviços</a> </li>
+                    <li><a href="<?=$base;?>/contato">Contato</a></li>
                     <li><a href="">Equipe</a></li>
                 </ul>
             </nav>
